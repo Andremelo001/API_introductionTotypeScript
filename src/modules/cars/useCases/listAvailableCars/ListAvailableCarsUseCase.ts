27c0d2c0652fb9669a427f869b59542c47dbbe94
Andremelo001 <andre.bezerra2@aluno.ce.gov.br>
2024-06-13 15:17:18 -0300

    feature: add car listing feature

---
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -0,0 +1,25 @@
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { inject, injectable } from "tsyringe";
+
+interface IResquest {
+    category_id?: string;
+    brand?: string;
+    name?: string;
+}
+
+@injectable()
+class ListAvailableCarsUseCase{
+    constructor(
+        @inject("CarsRepository")
+        private carsRepository: ICarsRepository
+    ){}
+    async execute({category_id, brand, name}: IResquest): Promise<Car[]> {
+
+        const cars = this.carsRepository.findAvailable(brand, category_id, name);
+
+        return cars;
+    }
+}
+
+export {ListAvailableCarsUseCase}
